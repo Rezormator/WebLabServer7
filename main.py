789973076a8647ad59d5530880726cb88db36aec
Rezor <kostiatsymbal2019@gmail.com>
2024-12-06 17:51:25 +0200

    improved

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import flask
-import flask_cors
-import datetime
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from datetime import datetime
 import pytz
 
 app = Flask(__name__)
@@ -61,4 +61,4 @@ def get_events():
     return jsonify(event_logs), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
